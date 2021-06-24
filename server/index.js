04cc2b6daa38e754520e97ebe66671abfb965233
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-06-24 22:59:30 +0900

    DB INSERT 확인 작업 완료

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "gdzc08180914!",
+  database: "crudtest",
+});
 
 app.get("/", (req, res) => {
-  res.send("hellooooooo youjin!");
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'goooooooooood');";
+  db.query(sqlInsert, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("run run run~");
+  });
 });
 app.listen(3001, () => {
   console.log("3001 running...");
