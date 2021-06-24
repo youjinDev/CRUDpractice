3f0df0a6125d90b67de159d98d2ee90ed6a976a4
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-06-24 21:51:35 +0900

    FrontEnd design

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <h1>CRUD APPLICATION</h1>
+      <div className="form">
+        <label>Movie Name :</label>
+        <input type="text" name="Movie" />
+        <label>Movie Review :</label>
+        <input type="text" name="Review" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
