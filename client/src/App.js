d9c631704340b0164181b1910c4217185b7d038a
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-06-25 19:20:53 +0900

    클라이언트에서 입력한 데이터 백으로 보내주기

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,46 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+
+  useState(() => {
+    // 여기에서 axios로 백엔드에 보냄
+  }, []);
+  const submitReview = () => {
+    axios
+      .post("http://localhost:3001/api/insert", {
+        movieName: movieName,
+        movieReview: review,
+      })
+      .then(() => {
+        alert("성공~!");
+      });
+  };
+
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
       <div className="form">
         <label>Movie Name :</label>
-        <input type="text" name="Movie" />
+        <input
+          type="text"
+          name="Movie"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Movie Review :</label>
-        <input type="text" name="Review" />
-        <button>Submit</button>
+        <input
+          type="text"
+          name="Review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        />
+        <button onClick={() => submitReview()}>Submit</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -10,15 +12,30 @@ const db = mysql.createConnection({
   database: "crudtest",
 });
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   const sqlInsert =
+//     "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('all or nothing', 'the best');";
+//   db.query(sqlInsert, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     res.send("run run run~");
+//   });
+// });
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
   const sqlInsert =
-    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'goooooooooood');";
-  db.query(sqlInsert, (err, result) => {
-    if (err) throw err;
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
     console.log(result);
-    res.send("run run run~");
   });
 });
+
 app.listen(3001, () => {
   console.log("3001 running...");
 });
