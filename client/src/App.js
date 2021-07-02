d42d093369330224f6ab59db74ef2a85b28edc47
YoujinCha <67622600+youjinDev@users.noreply.github.com>
2021-07-02 21:48:07 +0900

    CRUD read practice

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,36 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 function App() {
   const [movieName, setMovieName] = useState("");
   const [review, setReview] = useState("");
+  const [movieData, setMovieData] = useState([]);
+  const nameInput = useRef(null);
+  const reviewInput = useRef(null);
 
   const submitReview = () => {
-    axios
-      .post("http://localhost:3001/api/insert", {
-        movieName: movieName,
-        movieReview: review,
-      })
-      .then(() => {
-        console.log("성공");
-        alert("성공~!");
-      });
+    if (movieName === "" || review === "") return;
+    axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    });
   };
 
   useEffect(() => {
-    // 여기에서 axios로 백엔드에 보냄
-    submitReview();
+    // 여기에서 axios로 백엔드에서 받음
+    axios.get("http://localhost:3001/api/data").then((res) => {
+      setMovieData(res.data);
+    });
   }, []);
-
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
       <div className="form">
         <label>Movie Name :</label>
         <input
+          ref={nameInput}
+          value={movieName}
           type="text"
           name="Movie"
           onChange={(e) => {
@@ -37,13 +39,36 @@ function App() {
         />
         <label>Movie Review :</label>
         <input
+          ref={reviewInput}
+          value={review}
           type="text"
           name="Review"
           onChange={(e) => {
             setReview(e.target.value);
           }}
         />
-        <button onClick={() => submitReview()}>Submit</button>
+        <button
+          onClick={() => {
+            submitReview();
+            reviewInput.current.value = "";
+            nameInput.current.value = "";
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      <hr />
+      <h1>Reviews</h1>
+      <hr />
+      <div>
+        <ul>
+          {movieData.map((review) => (
+            <li key={review.id}>
+              <h1 className="title">{review.movieName}</h1>
+              <h3>{review.movieReview}</h3>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,16 +1,7 @@
+const mysql = require("mysql");
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const db = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "gdzc08180914!",
-  database: "crudtest",
-});
 
 // app.get("/", (req, res) => {
 //   const sqlInsert =
@@ -22,11 +13,20 @@ const db = mysql.createConnection({
 //   });
 // });
 
+const db = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "gdzc08180914!",
+  database: "crudtest",
+});
+
 app.use(express.json());
 app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true })) 은 deprecated 되어서 express에서 대체 가능
 app.use(express.urlencoded({ extended: true }));
 
+// db에 쓰기
 app.post("/api/insert", (req, res) => {
   const movieName = req.body.movieName;
   const movieReview = req.body.movieReview;
@@ -37,6 +37,7 @@ app.post("/api/insert", (req, res) => {
   });
 });
 
+//db에서 가져오기
 app.get("/api/data", (req, res) => {
   const sqlSelect = "SELECT * FROM movie_reviews";
   db.query(sqlSelect, (err, result, field) =>
